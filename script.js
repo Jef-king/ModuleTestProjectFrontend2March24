b6368aa577599c06208429218fa3ec815a1f796a
jefking <jefking@outlook.in>
2024-04-04 19:57:01 +0530

    html css completed

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-let
+let employees=[];
+function startAction(){
+    let name=document.querySelector("#name").value.trim();
+    let profession=document.querySelector("#profession").value.trim();
+    let age=document.querySelector("#age").value.trim();
+    let error=document.querySelector(".error");
+    let success=document.querySelector(".success");
+    console.log("Hello");
+    if((profession==='')||(name==='')||(age==='')){
+        error.innerText="Error: Please Make sure All the fields are filled before adding in an employee !";
+        success.innerText="";
+    }else{
+        success.innerText="Success: Employee Added !";
+        error.innerText="";
+    }
+
+}
